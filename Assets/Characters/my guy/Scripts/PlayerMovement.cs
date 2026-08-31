a07e37bcf15285114f45b2f0200aa283be7cea46
Benjamin Glasser <glasserben@gmail.com>
2026-08-31 15:05:36 -0700

    adds vertical movement

--- Assets/Characters/my guy/Scripts/PlayerMovement.cs
+++ Assets/Characters/my guy/Scripts/PlayerMovement.cs
@@ -1,34 +1,64 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
-    public Vector2 movementInput;
-    public Vector3 movementDirection;
-
     [SerializeField] private float speed = 10f;
+    [SerializeField] private float verticalSpeed = 2f;
     [SerializeField] private float rotationSpeed = 10f;
 
+    private Vector2 movementInput;
+    private float verticalInput;
 
-    public void OnMovement(InputValue value)
-        {
-            movementInput = value.Get<Vector2>();
-        }
+    private Rigidbody rb;
 
-
-    private void Update()
+    private void Start()
     {
-    //  use WASD to add Vector2 values to the X and Z component of transofrm on capsule character
-        movementDirection = new Vector3(movementInput.x, 0, movementInput.y);
-        movementDirection.Normalize();
-        
-        transform.Translate(movementDirection * Time.deltaTime * speed, Space.World);
-
-        if (movementDirection != Vector3.zero)
-        {
-            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
-            transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
-        }
+        rb = GetComponent<Rigidbody>();
     }
 
+    public void OnMovement(InputValue value)
+    {
+        movementInput = value.Get<Vector2>();
+    }
+
+    public void OnVertical(InputValue value)
+    {
+        verticalInput = value.Get<float>();
+    }
+
+    private void FixedUpdate()
+    {
+
+        Vector2 horizontalInput = Vector2.ClampMagnitude(movementInput, 1f);
+
+        // Create a direction from WASD and the vertical controls.
+        Vector3 movementDirection = new Vector3(horizontalInput.x, verticalInput, horizontalInput.y);
+
+        
+
+        Vector3 velocity = new Vector3(
+            movementDirection.x * speed,
+            movementDirection.y * verticalSpeed,
+            movementDirection.z * speed
+        );
+
+        rb.linearVelocity = velocity;
+
+        // Rotate only toward horizontal movement.
+        Vector3 horizontalDirection = new Vector3(movementDirection.x, 0f, movementDirection.z);
+
+        if (horizontalDirection != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(
+                horizontalDirection,
+                Vector3.up
+            );
+
+            Quaternion smoothRotation = Quaternion.Slerp(rb.rotation,targetRotation,rotationSpeed * Time.fixedDeltaTime);
+
+            rb.MoveRotation(smoothRotation);
+        }
+    }
 }
